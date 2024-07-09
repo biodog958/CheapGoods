63196736fdf89745f660361a27cdd283b5502453
liyuanjiang <912573012@qq.com>
2024-07-09 12:57:31 +0800

    创建工程

---
+++ MainCode/src/main/java/com/bugbuster/maincode/MainCodeApplication.java
@@ -0,0 +1,13 @@
+package com.bugbuster.maincode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainCodeApplication.class, args);
+    }
+
+}
---
+++ MainCode/src/test/java/com/bugbuster/maincode/MainCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugbuster.maincode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
